21a07cb4bff042460a4bc0da15467d1d48f42555
Erik Paiva <erik007br@hotmail.com>
2025-09-25 21:59:21 -0300

    Problema POST resolvido

--- listacontrole/views.py
+++ listacontrole/views.py
@@ -7,22 +7,14 @@ from django.http import HttpRequest
 
 def listHome(request: HttpRequest):
     if request.method == 'POST':
-        form_type = request.POST.get('form_type')
-        print(form_type)
-        if form_type == 'motorista':
+        print('teste')
+        if 'form1_submit' in request.POST:
             print('entrou motorista')
+        elif 'form2_submit' in request.POST:
+            print('entrou veiculo')
+        elif 'form3_submit' in request.POST:
+            print('entrou uso')
 
 
     context = {'form': ContactForm(), 'formveiculo': VeiculoForm(), 'formuso': UsoForm()}
     return render(request, 'listacontrole/home.html', context)
-
-
-def CadastroMotorista(request: HttpRequest):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return render(request, 'listacontrole/home.html', {'form': ContactForm(), 'formveiculo': VeiculoForm(), 'formuso': UsoForm()})
-    else:
-        form = ContactForm()
-    return render(request, 'listacontrole/home.html', {'form': form, 'formveiculo': VeiculoForm(), 'formuso': UsoForm()})
